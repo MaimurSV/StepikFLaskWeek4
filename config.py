04101635248b7b00af4889073009a84a52f5374d
MaimurSV <asoi4@yandex.ru>
2020-11-26 00:32:42 +0500

    Update config.py
    Изменен путь к БД для Heroku

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-import os
-
-
-# Для указания пути к файлу БД воспользумся путем до текущего модуля
-# - Текущая папка
-current_path = os.path.dirname(os.path.realpath(__file__))
-# - Путь к файлу БД в данной папке
-db_path = "sqlite:///" + current_path + "\\base.db"
+db_path = "sqlite:///base.db"
 
 
 class Config:
